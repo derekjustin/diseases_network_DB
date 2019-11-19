05e3e1724d43eb8a2c4cb5d6933ef81266342b54
Justin Strelka <jstrelka@student.cccs.edu>
2019-11-19 13:24:47 -0700

    labeling.cypher is created and cyphercreator.py is working properly

--- cyphercreator.py
+++ cyphercreator.py
@@ -1,7 +1,21 @@
 import json
+import os
+
+if os.path.exists("labeling.cypher"):
+    os.remove("labeling.cypher")
+
+labelcypherfile = open("labeling.cypher", "a")
 
 with open("nodes.json",encoding='utf-8-sig', errors='ignore') as f:
      contents = json.load(f, strict=False)
+f.close()
 
 for community in contents:
-    print(community['name'])
+    if (community["communitySize"] < 10):
+        for disease in community["members"]:
+            labelcypherfile.write("MATCH( node { code: '" + disease + "'} ) DETACH DELETE node\nWITH 1 as dummy\n")
+    else:
+        for disease in community["members"]:
+            labelcypherfile.write("MATCH( node { code: '" + disease + "'} ) SET node:c" + str(community["community"]) + "\nWITH 1 as dummy\n")
+
+labelcypherfile.close()
