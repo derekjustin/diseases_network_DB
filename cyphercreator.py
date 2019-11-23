c69d24fcb7b968ad04d18d63a9bf52fd87b1bddd
Justin Strelka <jstrelka@student.cccs.edu>
2019-11-23 07:30:36 -0700

    added commenting to cyphercreator.py

--- cyphercreator.py
+++ cyphercreator.py
@@ -9,21 +9,25 @@ import datetime
 
 currentDateAndTime = datetime.datetime.today()
 
-
+# Remove labeling.cypher if file exists
 if os.path.exists("labeling.cypher"):
     os.remove("labeling.cypher")
 
 labelCypherFile = open("labeling.cypher", "a")
 
-with open("nodes.json",encoding='utf-8-sig', errors='ignore') as f:
-     contents = json.load(f, strict=False)
-f.close()
+# Read in json file to contents
+with open("nodes.json",encoding='utf-8-sig', errors='ignore') as inFile:
+     contents = json.load(inFile, strict=False)
 
+inFile.close()
+
+# Print header lines in labeling.cypher file
 labelCypherFile.write("// AUTHORS: DEREK HOLSAPPLE,JUSTIN STRELKA\n\
 // DATE: " + str(currentDateAndTime) + "\n\
 // PROJECT: disease_network_DB\n\
 // FILE: LABELING.CYPHER\n")
 
+# Loop the contents of json file for community processing
 for community in contents:
     if (community["communitySize"] < 10):
         for disease in community["members"]:
